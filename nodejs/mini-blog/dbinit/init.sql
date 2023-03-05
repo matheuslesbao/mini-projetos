1d7842c7155023aabde119f89866c303b7d4e141
matheuslesbao <matheuss.lesbao@gmail.com>
2023-03-04 21:46:16 -0300

    comentando algumas coisas que nao irei usar no docker

--- nodejs/mini-blog/dbinit/init.sql
+++ nodejs/mini-blog/dbinit/init.sql
@@ -5,19 +5,9 @@ SET
 USE `blog-estudo`;
 
 CREATE TABLE
-  IF NOT EXISTS `usuarios` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `nome` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-  ) ENGINE = InnoDB;
-
-CREATE TABLE
-  IF NOT EXISTS `noticias` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `titulo` VARCHAR(255) NOT NULL,
-    `descricao` LONGTEXT NOT NULL,
-    `id_usuario` INT NULL,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `blog-estudo`.`usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-  ) ENGINE = InnoDB;
+  IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+  );
