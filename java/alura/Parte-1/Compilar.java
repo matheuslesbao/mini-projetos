f8f3d27b241cceaa6c67462b41430c9e67c5f03c
matheuslesbao <matheuss.lesbao@gmail.com>
2023-03-04 21:45:22 -0300

    estudando java

---
+++ java/alura/Parte-1/Compilar.java
@@ -0,0 +1,7 @@
+public class Compilar{
+
+    public static void main(String[] args){
+
+        System.out.println("Teste de Compilação funcionou!!, o nome da public class deve ser o mesmo do nome do Arquivo para funcionar.");
+    }
+}
